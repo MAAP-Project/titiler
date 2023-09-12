758720c04478e09303d17cccb30c584e18cf1f44
bick4ord <bick4ord@users.noreply.github.com>
2023-09-12 13:25:54 -0400

    add an optional env var config for mosaic lambda that will override t… (#22)
    * add an optional env var config for mosaic lambda that will override the host header in the event, so that responses crafted use the desired external-facing domain instead of internal API gateway
    Co-authored-by: Phil Varner <pvarner@element84.com>
    * Update deployment/aws/lambda/handler.py

--- deployment/aws/lambda/handler.py
+++ deployment/aws/lambda/handler.py
@@ -1,6 +1,7 @@
 """AWS Lambda handler."""
 
 import logging
+import os
 
 from mangum import Mangum
 
@@ -9,5 +10,13 @@ from titiler.application.main import app
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
 
-# mangum > 0.11.x removes the "log_level" parameter from the Mangum constructor
-handler = Mangum(app, lifespan="auto")
+REQUEST_HOST_HEADER_OVERRIDE_ENV_VAR = "REQUEST_HOST_HEADER_OVERRIDE"
+
+
+def handler(event, context):
+    """If env var is set, override the host header in the event passed to the lambda"""
+    if rhh := os.getenv(REQUEST_HOST_HEADER_OVERRIDE_ENV_VAR):
+        event["headers"]["host"] = rhh
+
+    asgi_handler = Mangum(app, lifespan="auto")
+    return asgi_handler(event, context)
