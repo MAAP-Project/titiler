295fdf27f456a15b91b45415c4a1fb8b2e88637c
Phil Varner <pvarner@element84.com>
2023-10-02 20:12:13 -0400

    fix pydantic warnings

--- src/titiler/mosaic/titiler/mosaic/models/stac_pydantic/shared.py
+++ src/titiler/mosaic/titiler/mosaic/models/stac_pydantic/shared.py
@@ -122,6 +122,6 @@ class Asset(StacCommonMetadata):
         Configuration values for the shared asset
         """
 
-        allow_population_by_field_name = True
+        populate_by_name = True
         use_enum_values = True
-        extra = Extra.allow
+        extra = 'allow'
